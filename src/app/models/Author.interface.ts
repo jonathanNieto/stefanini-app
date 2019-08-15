61d08868507bdb061ec21363efb173702dcc6718
Jonathan Nieto <jonathan.nieto.davila@gmail.com>
2019-08-15 23:33:19 +0000

    init app

---
+++ src/app/models/Author.interface.ts
@@ -0,0 +1,6 @@
+export class Author {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
---
+++ src/app/models/Publication.interface.ts
@@ -0,0 +1,7 @@
+export interface Publication {
+    id: number;
+    title: string;
+    description: string;
+    date: string;
+    authorId?: number;
+}
